e54b205e7a91560ca24d9718b8fe9f79f327b853
Bussonnier Matthias <bussonniermatthias@gmail.com>
2012-08-07 21:36:26 +0200

    replicate github tree structure

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from gist import app as gist
+from githubapp import app as github
+import os
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     debug = os.path.exists('.debug')
+    print 'url scheme' , os.environ.get('URL_SCHEME', None)
     if debug :
         print 'DEBUG MODE IS ACTIVATED !!!'
     else :
         print 'debug is not activated'
-    gist.run(host='0.0.0.0', port=port, debug=debug)
+    github.run(host='0.0.0.0', port=port, debug=debug)
---
+++ githubapp.py
@@ -0,0 +1,67 @@
+import os
+import base64
+from flask import Flask , request, render_template
+import nbconvert.nbconvert as nbconvert
+import requests
+from nbformat import current as nbformat
+from flask import Flask, redirect, abort
+import re
+import github as gh
+
+app = Flask(__name__)
+github = gh.Github()
+
+@app.route('/')
+def render_url():
+    return 'you are at root'
+
+@app.route('/<user>/')
+def user(user):
+    return github.get_user(user).name
+
+@app.route('/<user>/<repo>/')
+def repo(user,repo):
+    return github.get_user(user).get_repo(repo).url
+
+
+@app.route('/<user>/<repo>/<path:subfile>')
+def file(user,repo, subfile):
+    base  = "You are trying to access the file : %(file)s, from the %(repo)s repository of %(name)s"
+    user = github.get_user(user)
+    repo = user.get_repo(repo)
+
+    master = repo.master_branch
+    branch = [b for b in repo.get_branches() if b.name == master][0]
+
+    headtree = repo.get_git_tree(branch.commit.sha)
+
+
+
+    formated = base % { 'name':user.name,'repo':repo.url, 'file':subfile}
+    e = rwt(repo, branch.commit.sha, subfile.split('/'))
+    f = repo.get_git_blob(e.sha)
+
+    return base64.decodestring(f.content)
+
+#recursively walk tree....
+def rwt(repo,sha,path):
+    subpath = path[1:]
+    key = path[0]
+    nodes = repo.get_git_tree(sha).tree
+    for n in nodes :
+        if n.path == key:
+            if n.type == 'tree':
+                return rwt(repo, n.sha, subpath)
+            else :
+                return n
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.path.exists('.debug')
+    if debug :
+        print 'DEBUG MODE IS ACTIVATED !!!'
+    else :
+        print 'debug is not activated'
+    app.run(host='0.0.0.0', port=port, debug=debug)
