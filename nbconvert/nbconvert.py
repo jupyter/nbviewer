8170091df356470cc240e37188db2fa0d7b79777
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-03-01 13:51:33 +0100

    enable google analytics

--- nbconvert/nbconvert.py
+++ nbconvert/nbconvert.py
@@ -805,7 +805,22 @@ class ConverterHTML(Converter):
         return header
 
     def optional_footer(self):
-        lines = []
+
+        analytics = """<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38683231-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>"""
+
+        lines = [analytics]
         lines.extend([
             '</body>',
             '</html>',
