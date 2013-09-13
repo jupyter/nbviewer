ade665288bb82516027b3e6bc1d30e93094106f0
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-09-13 11:25:22 +0200

    add minimal heroku bootstrap script

---
+++ heroku-bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+heroku create $1
+heroku addons:add memcachier:dev --app $1
+heroku addons:add newrelic:standard --app $1
+heroku git:remote -r $1 --app $1
+heroku config:set LIBRARY_PATH=/app/.heroku/vendor/lib  --app $1
+heroku config:set LD_LIBRARY_PATH=/app/.heroku/vendor/lib  --app $1
+heroku config:set PATH=bin:app/.heroku/venv/bin:/bin:/usr/local/bin:/usr/bin --app $1
+heroku config:set BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git --app $1
+
+
