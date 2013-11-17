2a20708c6e23d3cf50b2704d58cdcb2072368642
MinRK <benjaminrk@gmail.com>
2013-11-17 14:17:39 -0800

    nbviewer.utils

---
+++ nbviewer/utils.py
@@ -0,0 +1,25 @@
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2013 The IPython Development Team
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+def url_path_join(*pieces):
+    """Join components of url into a relative url
+
+    Use to prevent double slash when joining subpath. This will leave the
+    initial and final / in place
+    """
+    initial = pieces[0].startswith('/')
+    final = pieces[-1].endswith('/')
+    stripped = [s.strip('/') for s in pieces]
+    result = '/'.join(s for s in stripped if s)
+    if initial:
+        result = '/' + result
+    if final:
+        result += '/'
+    if result == '//':
+        result = '/'
+    return result
+
