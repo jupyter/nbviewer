0762bdcc407ef2c1115d665b0bd4e35464989488
MinRK <benjaminrk@gmail.com>
2013-11-26 23:16:06 -0800

    restore old multipart memcache behavior

--- nbviewer/cache.py
+++ nbviewer/cache.py
@@ -58,20 +58,55 @@ class AsyncMemcache(object):
     """
     def __init__(self, *args, **kwargs):
         self.pool = kwargs.pop('pool', None) or ThreadPoolExecutor(1)
+        
         self.mc = pylibmc.Client(*args, **kwargs)
         self.mc_pool = pylibmc.ThreadMappedPool(self.mc)
     
     def get(self, *args, **kwargs):
         return self.pool.submit(self._threadsafe_get, *args, **kwargs)
     
-    def _threadsafe_get(self, *args, **kwargs):
+    def _threadsafe_get(self, key, *args, **kwargs):
         with self.mc_pool.reserve() as mc:
-            return mc.get(*args, **kwargs)
+            return mc.get(key, *args, **kwargs)
     
     def set(self, *args, **kwargs):
         return self.pool.submit(self._threadsafe_set, *args, **kwargs)
 
-    def _threadsafe_set(self, *args, **kwargs):
+    def _threadsafe_set(self, key, value, *args, **kwargs):
         with self.mc_pool.reserve() as mc:
-            return mc.set(*args, **kwargs)
+            return mc.set(key, value, *args, **kwargs)
+
+class AsyncMultipartMemcache(AsyncMemcache):
+    """subclass of AsyncMemcache that splits large files into multiple chunks
+    
+    because memcached limits record size to 1MB
+    """
+    def __init__(self, *args, **kwargs):
+        self.chunk_size = kwargs.pop('chunk_size', 950000)
+        self.max_chunks = kwargs.pop('max_chunks', 64)
+        super(AsyncMultipartMemcache, self).__init__(*args, **kwargs)
+    
+    def _threadsafe_get(self, key, *args, **kwargs):
+        keys = [b'%s.%i' % (key, idx) for idx in range(self.max_chunks)]
+        with self.mc_pool.reserve() as mc:
+            values = mc.get_multi(keys, *args, **kwargs)
+        parts = []
+        for key in keys:
+            if key not in values:
+                break
+            parts.append(values[key])
+        if parts:
+            return b''.join(parts)
+    
+    def _threadsafe_set(self, key, value, *args, **kwargs):
+        chunk_size = self.chunk_size
+        offsets = range(0, len(value), chunk_size)
+        app_log.info('storing cache in %i chunks' % len(offsets))
+        if len(offsets) > self.max_chunks:
+            raise ValueError("file is too large: %s" % len(value))
+        values = {}
+        for idx, offset in enumerate(offsets):
+            values[b'%s.%i' % (key, idx)] = value[offset:offset+chunk_size]
+        with self.mc_pool.reserve() as mc:
+            return mc.set_multi(values, *args, **kwargs)
 
