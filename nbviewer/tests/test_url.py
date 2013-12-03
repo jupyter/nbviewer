6fabf6b01842da9fd1458da62fbb7727244f685d
MinRK <benjaminrk@gmail.com>
2013-12-02 17:11:07 -0800

    unicode tests

---
+++ nbviewer/tests/test_url.py
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2013 The IPython Development Team
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+import requests
+
+from .base import NBViewerTestCase
+
+class URLTestCase(NBViewerTestCase):
+    def test_url(self):
+        url = self.url('url/jdj.mit.edu/~stevenj/IJulia Preview.ipynb')
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertIn('Download Notebook', r.text)
+
