6ffa0ccb4bd10851f27df6c8e9f22b377354db75
Kyle Kelley <kyle.kelley@rackspace.com>
2014-04-10 10:32:05 -0500

    Rely on 2.0.0 release

--- nbviewer/tests/test_github.py
+++ nbviewer/tests/test_github.py
@@ -12,11 +12,11 @@ from .base import NBViewerTestCase
 
 class GitHubTestCase(NBViewerTestCase):
     def ipython_example(self, *parts, **kwargs):
-        ref = kwargs.get('ref', 'master')
-        return self.url('github/ipython/ipython/blob/%s/examples/notebooks' % ref, *parts)
-    
+        ref = kwargs.get('ref', 'rel-2.0.0')
+        return self.url('github/ipython/ipython/blob/%s/examples' % ref, *parts)
+
     def test_github(self):
-        url = self.ipython_example('Part 1 - Running Code.ipynb')
+        url = self.ipython_example('Index.ipynb')
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
 
@@ -49,12 +49,12 @@ class GitHubTestCase(NBViewerTestCase):
         self.assertIn('/github/tlapicka/IPythonNotebooks/blob/', r.request.url)
 
     def test_github_tag(self):
-        url = self.ipython_example('Part 1 - Running Code.ipynb', ref='rel-1.0.0')
+        url = self.ipython_example('Index.ipynb', ref='rel-1.0.0')
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
     
     def test_github_commit(self):
-        url = self.ipython_example('Part 1 - Running Code.ipynb',
+        url = self.ipython_example('Index.ipynb',
             ref='02da31ca5a6576dfe9a95ecd9497c1df9a63533d'
         )
         r = requests.get(url)
@@ -62,62 +62,62 @@ class GitHubTestCase(NBViewerTestCase):
     
     def test_github_blob_redirect(self):
         url = self.url(
-            'urls/github.com/ipython/ipython/blob/rel-1.0.0/examples/notebooks',
-            'Part 1 - Running Code.ipynb',
+            'urls/github.com/ipython/ipython/blob/rel-2.0.0/examples',
+            'Index.ipynb',
         )
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
         # verify redirect
-        self.assertIn('/github/ipython/ipython/blob/rel-1.0.0', r.request.url)
+        self.assertIn('/github/ipython/ipython/blob/rel-2.0.0', r.request.url)
     
     def test_github_raw_redirect(self):
         url = self.url(
-            'urls/raw.github.com/ipython/ipython/rel-1.0.0/examples/notebooks',
-            'Part 1 - Running Code.ipynb',
+            'urls/raw.github.com/ipython/ipython/blob/rel-2.0.0/examples',
+            'Index.ipynb',
         )
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
         # verify redirect
-        self.assertIn('/github/ipython/ipython/blob/rel-1.0.0', r.request.url)
+        self.assertIn('/github/ipython/ipython/blob/rel-2.0.0/examples', r.request.url)
     
     def test_github_raw_redirect_2(self):
         """test /url/github.com/u/r/raw/ redirects"""
         url = self.url(
-            "url/github.com/ipython/ipython/raw/rel-1.0.0/examples/notebooks",
-            "Part 1 - Running Code.ipynb"
+            "url/github.com/ipython/ipython/blob/rel-2.0.0/examples",
+            "Index.ipynb"
         )
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
         # verify redirect
-        self.assertIn('/github/ipython/ipython/blob/rel-1.0.0', r.request.url)
+        self.assertIn('/github/ipython/ipython/blob/rel-2.0.0/examples', r.request.url)
     
     def test_github_repo_redirect(self):
         url = self.url("github/ipython/ipython")
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
         # verify redirect
-        self.assertIn('/github/ipython/ipython/tree/master', r.request.url)
+        self.assertIn('/github/ipython/ipython/tree/rel-2.0.0', r.request.url)
 
     def test_github_tree(self):
-        url = self.url("github/ipython/ipython/tree/master/IPython/")
+        url = self.url("github/ipython/ipython/tree/rel-2.0.0/IPython/")
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
         self.assertIn("__init__.py", r.text)
 
 
     def test_github_tree_redirect(self):
-        url = self.url("github/ipython/ipython/tree/master/MANIFEST.in")
+        url = self.url("github/ipython/ipython/tree/rel-2.0.0/MANIFEST.in")
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
         # verify redirect
-        self.assertIn('/github/ipython/ipython/blob/master', r.request.url)
+        self.assertIn('/github/ipython/ipython/blob/rel-2.0.0', r.request.url)
         self.assertIn('global-exclude', r.text)
 
     def test_github_blob_redirect(self):
-        url = self.url("github/ipython/ipython/blob/master/IPython")
+        url = self.url("github/ipython/ipython/blob/rel-2.0.0/IPython")
         r = requests.get(url)
         self.assertEqual(r.status_code, 200)
         # verify redirect
-        self.assertIn('/github/ipython/ipython/tree/master/IPython', r.request.url)
+        self.assertIn('/github/ipython/ipython/tree/rel-2.0.0/IPython', r.request.url)
         self.assertIn('__init__.py', r.text)
 
