7ac8d9f3bb74b202e50bec8329050d30094598ab
rgbkrk <rgbkrk@gmail.com>
2014-09-01 20:42:43 -0700

    Fix encoding, fix spacing.

--- nbviewer/search.py
+++ nbviewer/search.py
@@ -23,20 +23,21 @@ import uuid
 from elasticsearch import Elasticsearch
 
 class NoSearch():
-  def __init__(self, host=None, port=None):
-    pass
+    def __init__(self, host=None, port=None):
+        pass
 
-  def index_notebook(self, notebook_url, notebook_contents):
-    app_log.debug("Totally not indexing \"{}\"".format(notebook_url))
-    pass
+    def index_notebook(self, notebook_url, notebook_contents):
+        app_log.debug("Totally not indexing \"{}\"".format(notebook_url))
+        pass
   
 class ElasticSearch():
-  def __init__(self, host="127.0.0.1", port=9200):
-    self.elasticsearch = Elasticsearch([{'host':host, 'port':port}])
+    def __init__(self, host="127.0.0.1", port=9200):
+      self.elasticsearch = Elasticsearch([{'host':host, 'port':port}])
   
-  def index_notebook(self, notebook_url, notebook_contents):
-    notebook_id = uuid.uuid5(uuid.NAMESPACE_URL, notebook_url)
-    resp = self.elasticsearch.index(index='notebooks',
-                                    doc_type='ipynb',
-                                    body=notebook_contents,
-                                    id=notebook_id.hex)
+    def index_notebook(self, notebook_url, notebook_contents):
+        notebook_url = notebook_url.encode('utf-8')
+        notebook_id = uuid.uuid5(uuid.NAMESPACE_URL, notebook_url)
+        resp = self.elasticsearch.index(index='notebooks',
+                                        doc_type='ipynb',
+                                        body=notebook_contents,
+                                        id=notebook_id.hex)
