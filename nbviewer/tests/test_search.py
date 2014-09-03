dc5479910056963e8cc6bdadc3fda3c6538756ec
rgbkrk <rgbkrk@gmail.com>
2014-09-03 05:55:13 -0700

    Not really a test, test

--- nbviewer/tests/test_search.py
+++ nbviewer/tests/test_search.py
@@ -5,6 +5,7 @@ from .base import NBViewerTestCase
 from nbviewer import search
 
 class ElasticSearchTestCase(NBViewerTestCase):
-    def test_index_notebook(self):
+    def test_finish_notebook(self):
         elasticsearch = search.ElasticSearch(host="127.0.0.1", port=9200)
-        assert elasticsearch.elasticsearch.host == "127.0.0.1"
+        nb_contents = {}
+        public=False
