ded929c5e2e530c9787456fa0f64fba59b6a1e9c
rgbkrk <rgbkrk@gmail.com>
2014-09-03 11:50:59 -0700

    Stub test for indexing

--- nbviewer/tests/test_index.py
+++ nbviewer/tests/test_index.py
@@ -6,6 +6,4 @@ from nbviewer import index
 
 class ElasticSearchTestCase(NBViewerTestCase):
     def test_finish_notebook(self):
-        elasticsearch = index.ElasticSearch(host="127.0.0.1", port=9200)
-        nb_contents = {}
-        public=False
+        pass
