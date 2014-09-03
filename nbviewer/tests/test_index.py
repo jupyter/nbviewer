a3f438815580c7753d69d89910dd1701bd1dee0a
rgbkrk <rgbkrk@gmail.com>
2014-09-03 11:22:11 -0700

    Switch to index name in test "suite"

--- nbviewer/tests/test_index.py
+++ nbviewer/tests/test_index.py
@@ -2,10 +2,10 @@ from unittest import SkipTest
 
 from .base import NBViewerTestCase
 
-from nbviewer import search
+from nbviewer import index
 
 class ElasticSearchTestCase(NBViewerTestCase):
     def test_finish_notebook(self):
-        elasticsearch = search.ElasticSearch(host="127.0.0.1", port=9200)
+        elasticsearch = index.ElasticSearch(host="127.0.0.1", port=9200)
         nb_contents = {}
         public=False
