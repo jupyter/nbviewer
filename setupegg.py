f0ba490729279fa0cbeb9dedd6511c76280aa9c5
Kyle Kelley <rgbkrk@gmail.com>
2015-01-02 16:30:36 -0600

    Include pygments via bower

--- setupegg.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""Wrapper to run setup.py using setuptools."""
-
-# import setuptools and call the actual setup
-import setuptools
-try:
-    execfile('setup.py')
-except NameError:
-    exec(open('setup.py','rb').read())
