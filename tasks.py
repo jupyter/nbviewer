bd2463295a8723b254b4bec1877a068726ac2ae1
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-03-03 01:42:41 -0500

    responsive images for mobile, screenshot generator

--- tasks.py
+++ tasks.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
+import shutil
+import tempfile
+
 import invoke
+
 from IPython.html import DEFAULT_STATIC_FILES_PATH
 
 
@@ -47,3 +52,50 @@ def less(debug=False):
         invoke.run(tmpl.format(less_file, *args))
         for less_file in ["styles", "notebook"]
     ]
+
+
+@invoke.task
+def screenshots(root="http://localhost:5000/", dest="./screenshots"):
+    dest = os.path.abspath(dest)
+
+    script = """
+        root = "{root}"
+
+        urls = ({{name, url}} for name, url of {{
+            home: ""
+            dir: "github/ipython/ipython/tree/3.x/examples/"
+            user: "github/ipython/"
+            gists: "gist/fperez/"
+            notebook: "github/ipython/ipython/blob/3.x/examples/Notebook/Notebook%20Basics.ipynb"}})
+
+        screens = ({{name, w, h}} for name, [w, h] of {{
+            smartphone_portrait: [320, 480]
+            smartphone_landscape: [480, 320]
+            tablet_portrait: [768, 1024]
+            tablet_landscape: [1024, 768]
+            desktop_standard: [1280, 1024]
+            desktop_1080p: [1920, 1080]
+        }})
+        
+        casper.start root
+
+        casper.each screens, (_, screen) ->
+            @then ->
+                @viewport screen.w, screen.h, ->
+            _.each urls, (_, page) ->
+                @thenOpen root + page.url, ->
+                    @wait 1000
+                @then ->
+                    @echo "#{{page.name}} #{{screen.name}}"
+                    @capture "{dest}/#{{page.name}}-#{{screen.name}}.png"
+
+        casper.run()
+    """.format(root=root, dest=dest)
+    
+    tmpdir = tempfile.mkdtemp()
+    tmpfile = os.path.join(tmpdir, "screenshots.coffee")
+    with open(tmpfile, "w+") as f:
+        f.write(script)
+    invoke.run("casperjs test {script}".format(script=tmpfile))
+    
+    shutil.rmtree(tmpdir)
