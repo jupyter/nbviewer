3ed64cc411638992d2bebdf520ad3b51e2284bdb
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-03-13 20:21:55 -0400

    adding simple scheme for front page

--- nbviewer/tests/test_json.py
+++ nbviewer/tests/test_json.py
@@ -2,17 +2,44 @@ import os
 import json
 from unittest import TestCase
 
+from jsonschema import validate
+
+
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 
 class JSONTestCase(TestCase):
-    json = "nbviewer/frontpage.json"
+    json = None
+    schema = None
 
     def test_json(self):
+        if not self.json:
+            return
+
         try:
             json.load(open(os.path.join(ROOT, self.json), "r"))
         except Exception as err:
             self.fail("%s failed to parse:  %s" % (self.json, err))
+
+    def test_schema(self):
+        if not self.schema:
+            return
+
+        try:
+            data = json.load(open(os.path.join(ROOT, self.json), "r"))
+            schema = json.load(open(os.path.join(ROOT, self.schema), "r"))
+            validate(data, schema)
+        except Exception as err:
+            self.fail("%s failed to validate against %s:  %s" % (
+                self.json,
+                self.schema,
+                err
+            ))
+
+
+class FrontpageJSONTestCase(JSONTestCase):
+    json = "nbviewer/frontpage.json"
+    schema = "nbviewer/frontpage.schema.json"
 
 
 class BowerJSONTestCase(JSONTestCase):
