84fd05e81eb714a0ad8cf12edf64958030a19d88
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-06-25 23:02:25 -0400

    adding tests for #471, #472, #473

---
+++ nbviewer/tests/test_security.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2015 The Jupyter Development Team
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+import requests
+
+from .base import NBViewerTestCase
+
+from ..providers.local.tests.test_localfile import (
+    LocalFileDefaultTestCase as LFDTC
+)
+
+class XSSTestCase(NBViewerTestCase):
+    def _xss(self, path, pattern='<script>alert'):
+        r = requests.get(self.url() + path)
+        self.assertEqual(r.status_code, 200)
+        self.assertNotIn(pattern, r.content)
+
+    def test_github_dirnames(self):
+        self._xss(
+            '/github/bburky/xss/tree/%3Cscript%3Ealert(1)%3C%2fscript%3E/'
+        )
+
+    def test_gist_filenames(self):
+        self._xss('/gist/bburky/c020825874798a6544a7')
+
+
+class LocalDirectoryTraversalTestCase(LFDTC):
+    def test_url(self):
+        ## assumes being run from base of this repo
+        url = self.url('localfile/../../foo.txt')
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 404)
