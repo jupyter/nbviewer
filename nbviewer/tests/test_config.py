f06cd6ab2fb16a2c09b73fe376c3cb3d44c1f5a0
Jonathan Frederic <jon.freder@gmail.com>
2015-11-11 13:58:18 -0800

    Add non-conflicting env test.

--- nbviewer/tests/test_config.py
+++ nbviewer/tests/test_config.py
@@ -2,8 +2,9 @@ import os
 from subprocess import Popen
 from .base import NBViewerTestCase
 import sys
-
 import requests
+from tornado.ioloop import IOLoop
+from nbviewer.app import main
 
 tmpl_fixture = "nbviewer/tests/templates"
 
@@ -22,3 +23,18 @@ class TemplatePathCLITestCase(NBViewerTestCase, CustomTemplateStub):
         return super(TemplatePathCLITestCase, cls).get_server_args() + [
             '--template_path={}'.format(tmpl_fixture),
         ]
+
+
+class TemplatePathEnvTestCase(NBViewerTestCase, CustomTemplateStub):
+
+    @classmethod
+    def _server_main(cls):
+        cls._server_loop = loop = IOLoop()
+        loop.make_current()
+        cls._server_loop.add_callback(cls._start_evt.set)
+
+        # Set environment variable
+        os.environ['NBVIEWER_TEMPLATE_PATH'] = tmpl_fixture
+
+        main(['', '--port=%d' % cls.port] + cls.get_server_args())
+        loop.close(all_fds=True)
