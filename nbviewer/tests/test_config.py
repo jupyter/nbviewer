dab13cbe857af149882d3935cc4e63873bd036ad
Jonathan Frederic <jon.freder@gmail.com>
2015-11-11 11:01:02 -0800

    Fix tests post rebase on #539

--- nbviewer/tests/test_config.py
+++ nbviewer/tests/test_config.py
@@ -1,6 +1,7 @@
 import os
 from subprocess import Popen
 from .base import NBViewerTestCase
+import sys
 
 import requests
 
@@ -17,21 +18,7 @@ class CustomTemplateStub(object):
 
 class TemplatePathCLITestCase(NBViewerTestCase, CustomTemplateStub):
     @classmethod
-    def get_server_cmd(cls):
-        return super(TemplatePathCLITestCase, cls).get_server_cmd() + [
+    def get_server_args(cls):
+        return super(TemplatePathCLITestCase, cls).get_server_args() + [
             '--template_path={}'.format(tmpl_fixture),
         ]
-
-
-class TemplatePathEnvTestCase(NBViewerTestCase, CustomTemplateStub):
-    @classmethod
-    def setup_class(cls):
-        server_cmd = cls.get_server_cmd()
-        devnull = open(os.devnull, 'w')
-        cls.server = Popen(
-            server_cmd,
-            stdout=devnull,
-            stderr=devnull,
-            env=dict(os.environ, NBVIEWER_TEMPLATE_PATH=tmpl_fixture)
-        )
-        cls.wait_until_alive()
