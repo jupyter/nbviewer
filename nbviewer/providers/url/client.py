de606b825fbbe2f2f2c75199d80ba65008debbad
Min RK <benjaminrk@gmail.com>
2016-02-16 11:19:55 +0100

    coroutinify http client
    fewer callbacks, moar coroutines

--- nbviewer/providers/url/client.py
+++ nbviewer/providers/url/client.py
@@ -1,9 +1,11 @@
-#-----------------------------------------------------------------------------
-#  Copyright (C) 2013 The IPython Development Team
-#
-#  Distributed under the terms of the BSD License.  The full license is in
-#  the file COPYING, distributed as part of this software.
-#-----------------------------------------------------------------------------
+"""Async HTTP client with bonus features!
+
+- Support caching via upstream 304 with ETag, Last-Modified
+- Log request timings for profiling
+"""
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
 
 import hashlib
 import pickle
@@ -14,8 +16,10 @@ from tornado.log import app_log
 
 from tornado import gen
 
+from nbviewer.utils import time_block
+
 #-----------------------------------------------------------------------------
-# Async GitHub Client
+# Async HTTP Client
 #-----------------------------------------------------------------------------
 
 # cache headers and their response:request mapping
@@ -28,77 +32,95 @@ cache_headers = {
 }
 
 class NBViewerAsyncHTTPClient(object):
-    """Subclass of AsyncHTTPClient with bonus logging and caching!"""
+    """Subclass of AsyncHTTPClient with bonus logging and caching!
     
-    def __init__(self, cache=None):
-        self.cache = cache
+    If upstream servers support 304 cache replies with the following headers:
+    
+    - ETag : If-None-Match
+    - Last-Modified : If-Modified-Since
+    
+    Upstream requests are still made every time,
+    but resources and rate limits may be saved by 304 responses.
+    
+    Currently, responses are cached for a non-configurable two hours.
+    """
+    
+    cache = None
+    expiry = 7200
     
     def fetch_impl(self, request, callback):
-        self.io_loop.add_callback(lambda : self._cached_fetch(request, callback))
+        self.io_loop.add_callback(lambda : self._fetch_impl(request, callback))
     
     @gen.coroutine
-    def _cached_fetch(self, request, callback):
+    def _fetch_impl(self, request, callback):
         tic = time.time()
         if request.user_agent is None:
             request.user_agent = 'Tornado-Async-Client'
-
-        without_params = request.url.split('?')[0]
+        
+        # when logging, use the URL without params
+        name = request.url.split('?')[0]
         cached_response = None
-        app_log.debug("Fetching %s", without_params)
+        app_log.debug("Fetching %s", name)
         cache_key = hashlib.sha256(request.url.encode('utf8')).hexdigest()
-        if self.cache:
-            cache_tic = time.time()
-            try:
-                cached_pickle = yield self.cache.get(cache_key)
-                if cached_pickle:
-                    cached_response = pickle.loads(cached_pickle)
-            except Exception:
-                app_log.error("Failed to get cached response for %s",
-                    without_params, exc_info=True)
+        with time_block("Upstream cache get %s" % name):
+            cached_response = yield self._get_cached_response(cache_key, name)
+        
         if cached_response:
-            app_log.info("Have cached response for %s", without_params)
-            dt = time.time() - cache_tic
-            app_log.debug("Upstream response cache hit for %s in %.2f ms",
-                without_params, 1e3 * dt)
+            app_log.debug("Upstream cache hit %s", name)
             # add cache headers, if any
             for resp_key, req_key in cache_headers.items():
                 value = cached_response.headers.get(resp_key)
                 if value:
                     request.headers[req_key] = value
+        else:
+            app_log.debug("Upstream cache miss %s", name)
         
-        @gen.coroutine
-        def finish(response):
-            dt = time.time() - tic
-            log = app_log.info if dt > 1 else app_log.debug
-            if response.code == 304 and cached_response:
-                log("Upstream 304 on %s in %.2f ms", without_params, 1e3 * dt)
-                callback(cached_response)
-            else:
-                if not response.error:
-                    log("Fetched  %s in %.2f ms", without_params, 1e3 * dt)
-                callback(response)
-                if self.cache:
-                    yield self.cache_response(response)
-                yield self.
-                if self.cache and not response.error and any(response.headers.get(key) for key in cache_headers):
-                    # cache for two hours if there are any cache headers (use cache expiry?)
-                    cache_tic = time.time()
-                    expiry = 7200
-                    try:
-                        pickle_response = pickle.dumps(response, pickle.HIGHEST_PROTOCOL)
-                        yield self.cache.set(
-                            cache_key,
-                            pickle_response,
-                            int(time.time() + expiry),
-                        )
-                    except Exception:
-                        app_log.error("Failed to cache response for %s" % without_params, exc_info=True)
-                    else:
-                        dt = time.time() - cache_tic
-                        app_log.debug("Cached upstream response for %s in %.2f ms",
-                            without_params, 1e3 * dt)
-        
-        super(NBViewerAsyncHTTPClient, self).fetch_impl(request, finish)
+        response = yield gen.Task(super(NBViewerAsyncHTTPClient, self).fetch_impl, request)
+        dt = time.time() - tic
+        log = app_log.info if dt > 1 else app_log.debug
+        if response.code == 304 and cached_response:
+            log("Upstream 304 on %s in %.2f ms", name, 1e3 * dt)
+            callback(cached_response)
+        else:
+            if not response.error:
+                log("Fetched  %s in %.2f ms", name, 1e3 * dt)
+            callback(response)
+            if not response.error:
+                yield self._cache_response(cache_key, name, response)
+    
+    @gen.coroutine
+    def _get_cached_response(self, cache_key, name):
+        """Get the cached response, if any"""
+        if not self.cache:
+            return
+        try:
+            cached_pickle = yield self.cache.get(cache_key)
+            if cached_pickle:
+                raise gen.Return(pickle.loads(cached_pickle))
+        except gen.Return:
+            raise # FIXME: remove gen.Return when we drop py2 support
+        except Exception:
+            app_log.error("Upstream cache get failed %s", name, exc_info=True)
+    
+    @gen.coroutine
+    def _cache_response(self, cache_key, name, response):
+        """Cache the response, if any cache headers we understand are present."""
+        if not self.cache:
+            return
+        if not any(response.headers.get(key) for key in cache_headers):
+            # no cache headers, no point in caching the response
+            return
+        with time_block("Upstream cache set %s" % name):
+            # cache the response if there are any cache headers (use cache expiry?)
+            try:
+                pickle_response = pickle.dumps(response, pickle.HIGHEST_PROTOCOL)
+                yield self.cache.set(
+                    cache_key,
+                    pickle_response,
+                    int(time.time() + self.expiry),
+                )
+            except Exception:
+                app_log.error("Upstream cache failed %s" % name, exc_info=True)
 
 
 class NBViewerSimpleAsyncHTTPClient(NBViewerAsyncHTTPClient, SimpleAsyncHTTPClient):
