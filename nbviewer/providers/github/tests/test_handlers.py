c019e1ace691561184f1fc8fe643a6c74c0faf61
Drunkar <drunkars.p@gmail.com>
2016-04-02 08:23:12 +0900

    Add rewrite rules for Github Enterprise.

--- nbviewer/providers/github/tests/test_handlers.py
+++ nbviewer/providers/github/tests/test_handlers.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+import os
 from unittest import TestCase
 
 from ....utils import transform_ipynb_uri
@@ -11,6 +12,13 @@ uri_rewrite_list = uri_rewrites()
 class TestRewrite(TestCase):
     def assert_rewrite(self, uri, rewrite):
         new = transform_ipynb_uri(uri, uri_rewrite_list)
+        self.assertEqual(new, rewrite)
+
+    def assert_rewrite_ghe(self, uri, rewrite):
+        os.environ['GITHUB_API_URL'] = 'https://example.com/api/v3/'
+        uri_rewrite_ghe_list = uri_rewrites()
+        os.environ.pop('GITHUB_API_URL', None)
+        new = transform_ipynb_uri(uri, uri_rewrite_ghe_list)
         self.assertEqual(new, rewrite)
 
     def test_githubusercontent(self):
@@ -47,3 +55,18 @@ class TestRewrite(TestCase):
         uri = u'username'
         rewrite = u'/github/username/'
         self.assert_rewrite(uri, rewrite)
+
+    def test_ghe_blob(self):
+        uri = u'https://example.com/user/reopname/blob/deadbeef/a mřížka.ipynb'
+        rewrite = u'/github/user/reopname/blob/deadbeef/a mřížka.ipynb'
+        self.assert_rewrite_ghe(uri, rewrite)
+
+    def test_ghe_raw_uri(self):
+        uri = u'https://example.com/user/reopname/raw/deadbeef/a mřížka.ipynb'
+        rewrite = u'/github/user/reopname/blob/deadbeef/a mřížka.ipynb'
+        self.assert_rewrite_ghe(uri, rewrite)
+
+    def test_ghe_tree(self):
+        uri = u'https://example.com/user/reopname/tree/deadbeef/a mřížka.ipynb'
+        rewrite = u'/github/user/reopname/tree/deadbeef/a mřížka.ipynb'
+        self.assert_rewrite_ghe(uri, rewrite)
