e52283ae3f30bc368458c4e128d372fd2e0461ec
Peter Parente <peter.parente@maxpoint.com>
2017-06-30 13:15:55 -0500

    Use unittest.TestCase.assertIn

--- nbviewer/providers/url/tests/test_url.py
+++ nbviewer/providers/url/tests/test_url.py
@@ -4,6 +4,7 @@
 #  Distributed under the terms of the BSD License.  The full license is in
 #  the file COPYING, distributed as part of this software.
 #-----------------------------------------------------------------------------
+import unittest
 
 import requests
 
@@ -13,7 +14,9 @@ class URLTestCase(NBViewerTestCase):
     def test_url(self):
         url = self.url('url/jdj.mit.edu/~stevenj/IJulia Preview.ipynb')
         r = requests.get(url)
-        self.assertIn(r.status_code, (200, 202))
+        # Base class overrides assertIn to do unicode in unicode checking
+        # We want to use the original unittest implementation
+        unittest.TestCase.assertIn(self, r.status_code, (200, 202))
         self.assertIn('Download Notebook', r.text)
 
 
