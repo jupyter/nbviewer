942cc47bc425a68342395f828368b5f407881436
William Krinsman <krinsman@berkeley.edu>
2020-03-24 15:58:47 -0700

    Fix STDERR from appearing with test_content, remove Asyncnbviewertestcase because it's never used

--- nbviewer/providers/url/tests/test_content.py
+++ nbviewer/providers/url/tests/test_content.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from ....tests.async_base import AsyncNbviewerTestCase
+import requests
+from ....tests.base import NBViewerTestCase
 
-
-class ForceUTF8TestCase(AsyncNbviewerTestCase):
+class ForceUTF8TestCase(NBViewerTestCase):
     def test_utf8(self):
         """ #507, bitbucket returns no content headers, but _is_ serving utf-8
         """
-        response = self.fetch(
-            '/urls/bitbucket.org/sandiego206/asdasd/raw/master/Untitled.ipynb'
+        response = requests.get(
+            self.url('/urls/bitbucket.org/sandiego206/asdasd/raw/master/Untitled.ipynb')
         )
-        self.assertEqual(response.code, 200)
-        self.assertIn("ñ", response.body)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("ñ", response.content)
