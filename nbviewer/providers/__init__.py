2ab493a019710b51b1877de6f78fab72973baf1c
Dolf Andringa <dolfandringa@gmail.com>
2022-05-19 14:39:45 +0800

    Added s3 provider

--- nbviewer/providers/__init__.py
+++ nbviewer/providers/__init__.py
@@ -6,12 +6,12 @@
 # -----------------------------------------------------------------------------
 
 default_providers = [
-    "nbviewer.providers.{}".format(prov) for prov in ["url", "github", "gist"]
+    "nbviewer.providers.{}".format(prov) for prov in ["url", "github", "gist", "s3"]
 ]
 
 default_rewrites = [
     "nbviewer.providers.{}".format(prov)
-    for prov in ["gist", "github", "dropbox", "huggingface", "url"]
+    for prov in ["gist", "github", "dropbox", "huggingface", "s3", "url"]
 ]
 
 
