9a49c7719d74bf98415d62288b161ba0015ee60d
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-10-31 10:03:19 +0100

    test

--- tasks.py
+++ tasks.py
@@ -96,6 +96,7 @@ def less(ctx, debug=False):
     args = (extra, NOTEBOOK_STATIC_PATH)
 
     for less_file in ["styles", "notebook", "slides", "custom"]:
+        print(tmpl.format(less_file, *args))
         ctx.run(tmpl.format(less_file, *args))
 
 
