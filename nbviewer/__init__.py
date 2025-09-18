ebe541768ba8405b13e2124ef122de28cd81e3d0
Min RK <benjaminrk@gmail.com>
2025-09-17 21:31:57 -0700

    PackageNotFoundError

--- nbviewer/__init__.py
+++ nbviewer/__init__.py
@@ -2,5 +2,5 @@ import importlib.metadata
 
 try:
     __version__ = importlib.metadata.version(__name__)
-except importlib.metadata.PackageNotFound:
+except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0.dev-unknown"
